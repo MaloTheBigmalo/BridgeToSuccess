957d5a6e6e2ae9be3c4015803051fab227f35e69
MaloTheBigmalo <ubuntu@b6b4cb624ef>
2019-02-24 18:00:39 +0000

    Création de la  fonction factoriel

--- factoriel.py
+++ factoriel.py
@@ -1,5 +1,15 @@
+
+def factoriel(n):
+    if n == 0:
+        return 1
+    else:
+        return n*factoriel(n-1)
+
+
 n = int(input("Taper l'entier "))
-P = 1
-for i in range(1,n+1):
-    P = P*i
-print("le factoriel de n",n," égale ",P)
+#P = 1
+#for i in range(1,n+1):
+#    P = P*i
+#print("le factoriel de n",n," égale ",P)
+print("le factoriel de ",n, "égale :",end="")
+print(factoriel(n))
