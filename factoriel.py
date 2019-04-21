50252b7ca4c8a2fb1a7d3af97bc8c57f379ffba5
MaloTheBigmalo <sitamalonda@hotmail.com>
2019-04-21 16:22:18 +0200

    changed some lines

--- factoriel.py
+++ factoriel.py
@@ -7,9 +7,5 @@ def factoriel(n):
 
 
 n = int(input("Taper l'entier "))
-#P = 1
-#for i in range(1,n+1):
-#    P = P*i
-#print("le factoriel de n",n," égale ",P)
 print("le factoriel de ",n, "égale :",end="")
 print(factoriel(n))
